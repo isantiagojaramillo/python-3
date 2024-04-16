0b064d6eb5dfb28cf3ce9d75077ec67110759ce1
isantiagojaramillo <sajaramillogr@cesde.net>
2024-04-15 20:28:16 -0500

    Conversion (casting)

---
+++ 02-Conversiones(casting).py
@@ -0,0 +1,23 @@
+numero1 = "35";
+numero2 = "18";
+
+# Concatenando variables
+print(numero1 + numero2);
+
+# Casting (conversión)
+num1 = int(numero1);
+num2 = int(numero2);
+
+print(num1 + num2);
+
+sueldo = 1200.43;
+sueldoEntero = int(sueldo);
+print(sueldoEntero);
+
+valor = "4500.89";
+valorDecimal = float(valor);
+print(valorDecimal);
+print(valorDecimal * 3);
+
+edad = 100;
+print(len(str(edad)));
