59650efe484d1f831af3051f553d6a81afb92720
isantiagojaramillo <sajaramillogr@cesde.net>
2024-04-27 20:41:01 -0500

    funciones

---
+++ 11-funciones.py
@@ -0,0 +1,31 @@
+# print("santi");
+# print("jara");
+# print("Developer");
+
+# print("santi");
+# print("jara");
+# print("Developer");
+
+# print("santi");
+# print("jara");
+# print("Developer");
+
+def saludar():
+    print("santi");
+    print("jara");
+    print("Developer");
+    return "Hola";
+
+# saludar();
+# saludar();
+# saludar();
+
+# print(saludar());
+
+def evaluarSueldoMinimo(sueldo):
+    if sueldo >= 1300000:
+        print("Cumples con el sueldo");
+    else:
+        print("Ganas menos del sueldo minimo");
+
+evaluarSueldoMinimo(800000);
