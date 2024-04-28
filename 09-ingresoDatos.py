dee8f8814b1ecf6b53694cd1b5257050a0f438e2
isantiagojaramillo <sajaramillogr@cesde.net>
2024-04-27 20:12:37 -0500

    Ingreso de datos

---
+++ 09-ingresoDatos.py
@@ -0,0 +1,15 @@
+# nombre ="Santi";
+# edad = 25;
+
+# print("Hola "+ nombre);
+# print("Tu edad es: ", edad);
+
+nombre = input("Ingrese su nombre: ");
+edad = int(input("Ingrese su edad: "));
+sueldo = float(input("Ingrese sueldo: "));
+
+edadFutura = edad + 20;
+
+print("Hola "+ nombre);
+print("Tu edad (dentro de 20 años será): ", edadFutura);
+print("Su salario es: ");
