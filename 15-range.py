36a3920cc14be9d1153ece99be35f498bb45aa61
isantiagojaramillo <sajaramillogr@cesde.net>
2024-05-12 20:02:37 -0500

    range

---
+++ 15-range.py
@@ -0,0 +1,11 @@
+# range() crea una lista inmutable de números enteros en sucesión aritmetica
+
+numeros = range(5); # [0,1,2,3,4]
+
+print(numeros);
+
+numeros1 = range(4, 10); # [4,5,6,7,8,9]
+print(numeros1[3]);
+
+numeros2 = range(10, 100, 8); # [10,18,26,34,42,50,58,66,74,82,90,98]
+print(numeros2[9]);
