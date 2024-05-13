b947d21507ad62e7b6206a01ce0824439263b317
isantiagojaramillo <sajaramillogr@cesde.net>
2024-05-12 20:31:46 -0500

    bucle for

---
+++ 16-for.py
@@ -0,0 +1,10 @@
+#Bucles: Son estructuras de control de flujo que repiten 1 o varias lineas de código
+
+# for numero in range(0, 20, 2):
+#     print("Valor actual: {} ".format(numero));
+
+# for i in range(0, 13):
+#     print("{0} * {1} = {2}".format(i, i, (i * i)));
+
+for nombre in ["Karen", "Oscar", "Hector", "Leonardo"]:
+    print("Cantidad de letras de {} es: {}".format(nombre, len(nombre)));
