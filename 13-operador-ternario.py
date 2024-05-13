ce31410de6c9a851f6b483a7169e075d0fc8d841
isantiagojaramillo <sajaramillogr@cesde.net>
2024-05-12 19:28:34 -0500

    operador ternario

---
+++ 13-operador-ternario.py
@@ -0,0 +1,13 @@
+"""
+String sexo;
+sexo = (10 > 10) ? "Masculino" : "Femenino";
+
+"""
+sexos = ("Hombre", "Mujer");
+
+posicion =  True;
+sexo = sexos(posicion); #Mujer
+print(sexo)
+
+sexo = sexos(not posicion); #Hombre
+print(sexo);
