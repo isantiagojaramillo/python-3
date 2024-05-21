15dee9dab477f0e7938950991976308f6a77baf4
isantiagojaramillo <sajaramillogr@cesde.net>
2024-05-20 20:57:04 -0500

    while

---
+++ 18-while.py
@@ -0,0 +1,16 @@
+#While: Estructura repetitiva que nos permite realizar multiples
+# iteraciones basandonos en el resultado de una expresion logica 
+# que puede tener como resultado un valor True o False.
+
+# indice = 1;
+
+# while indice < 10:
+#     print("Valor actual: {}".format(indice));
+#     indice += 1;
+
+
+inicio = 2;
+
+while inicio <= 100:
+    print("Numero par: {}".format(inicio));
+    inicio += 2;
