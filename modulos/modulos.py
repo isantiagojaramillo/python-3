b06c196cefdff4db164605b02c201f3d89d4fcec
isantiagojaramillo <sajaramillogr@cesde.net>
2024-05-27 14:39:52 -0500

    modulos

---
+++ modulos/modulos.py
@@ -0,0 +1,20 @@
+"""
+Módulo:
+Es un archivo con extensión .py o .pyc(Python compiled), que
+posee su propio espacio y que puede contener variables, funciones,
+clases o incluso otros módulos.
+
+¿Para qué sirve?
+Para organizar mejor el código y poder reutilizar mejor.
+Modularización y reutilización.
+
+"""
+# import funcionesMatematicas;
+# import funcionesMatematicas as fm;
+# from modulos.funcionesMatematicas import sumar, multiplicar;
+from modulos.funcionesMatematicas import *;
+
+# print(funcionesMatematicas.sumar(5,6));
+# print(fm.sumar(5,6));
+sumar(5, 6);
+multiplicar(5,6);
